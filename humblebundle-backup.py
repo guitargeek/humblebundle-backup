377641ee1401f4716fdff71a5b94a962b8bb7ebd
Jonas Rembser <jonas.rembser@cern.ch>
2019-06-30 11:18:05 +0200

    python-modernize

--- humblebundle-backup.py
+++ humblebundle-backup.py
@@ -33,6 +33,9 @@ Usage: enter the content of your authentication cookie ("_simpleauth_sess"
 Edit this
 """
 # The content of the auth-cookie should be a huge string with random letters and numbers.
+from __future__ import absolute_import
+from __future__ import print_function
+from six.moves import input
 auth = ""
 # Insert you target directory of choice. Multiple subfolders corresponding
 # to the different Games will be created in the target directory.
@@ -42,8 +45,8 @@ target_dir = ""
 Now the real program starts
 """
 
-import requests, cookielib, os, sys, json, urllib2
-from urlparse import urljoin, urlsplit
+import requests, six.moves.http_cookiejar, os, sys, json, six.moves.urllib.request, six.moves.urllib.error, six.moves.urllib.parse
+from six.moves.urllib.parse import urljoin, urlsplit
 
 URL = "https://www.humblebundle.com"
 LOGIN_URL = 'https://www.humblebundle.com/login'
@@ -55,7 +58,7 @@ STORE_URL = 'https://www.humblebundle.com/store/api/humblebundle'
 
 def download_file(opened_url, file_size, target_file_name):
     f = open(target_file_name, 'wb')
-    print u"Downloading: {0}, Size: {1:.1f} MB.".format(target_file_name, file_size / 1024.0**2)
+    print(u"Downloading: {0}, Size: {1:.1f} MB.".format(target_file_name, file_size / 1024.0**2))
     file_size_dl = 0
     block_sz = 8192
     while True:
@@ -67,16 +70,16 @@ def download_file(opened_url, file_size, target_file_name):
         f.write(buffer)
         status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
         status = status + chr(8)*(len(status)+1)
-        print status,
+        print(status, end=' ')
 
     f.close()
 
 if auth == "" or auth == None:
-    print "Authentication cookie content missing!"
+    print("Authentication cookie content missing!")
     sys.exit()
 
 if target_dir == None:
-    print "Target dir unspecified!"
+    print("Target dir unspecified!")
     sys.exit()
 
 list_missing_only = "--list-missing-only" in sys.argv
@@ -86,37 +89,37 @@ if target_dir != "" and target_dir[-1] != "/":
     target_dir = target_dir + "/"
 
 # Create a cookiejar with the authentication cookie inside
-cookiejar = cookielib.MozillaCookieJar()
+cookiejar = six.moves.http_cookiejar.MozillaCookieJar()
 expires = int(auth.split('|')[1]) + 730 * 24 * 60 * 60
-cookie = cookielib.Cookie(version = 0, name = '_simpleauth_sess', value = auth,
+cookie = six.moves.http_cookiejar.Cookie(version = 0, name = '_simpleauth_sess', value = auth,
           port = None, port_specified = False, domain = urlsplit(URL)[1],
           domain_specified = False, domain_initial_dot = False, path = '/',
           path_specified = False, secure = True, expires = expires,
           discard = False, comment = None, comment_url = None, rest={},)
 cookiejar.set_cookie(cookie)
 
-print "Get list with all claimed entities from humblebundle.com..."
+print("Get list with all claimed entities from humblebundle.com...")
 r = requests.get(CLAIMED_ENTITIES_URL, cookies=cookiejar)
 data = json.loads(r.text.replace("\u00fc", "u"))
 
 target_folder_dict = {}
 
 if not list_missing_only:
-    print "Calculating total library size.."
+    print("Calculating total library size..")
     total_size = 0
     for item in data["Downloads"]:
         machine_name = item["machine_name"]
         for download in item["download_struct"]:
-            if "file_size" in download.keys():
+            if "file_size" in list(download.keys()):
                 total_size = total_size + download["file_size"]
 
-    proceed = raw_input("Total library size will be {0:.2f} GB. Continue? [y/n]: ".format(total_size / 1024.0**3))
+    proceed = input("Total library size will be {0:.2f} GB. Continue? [y/n]: ".format(total_size / 1024.0**3))
     while proceed.lower() != "y":
         if proceed.lower() == "n":
             sys.exit()
-        proceed = raw_input("Thats not a valid option. Continue? [y/n]: ")
+        proceed = input("Thats not a valid option. Continue? [y/n]: ")
 
-print "Updating directory structure..."
+print("Updating directory structure...")
 for item in data["SubProducts"]:
     human_name = item["human_name"]
     # Creating a directory with the human readable name, if it does not extist
@@ -137,7 +140,7 @@ if not list_missing_only:
             file_name = target_dir + human_name + "/" + [i for i in data["Downloads"] if i["machine_name"] == machine_name][0]["download_struct"][0]["url"]["web"]
             if os.path.isfile(file_name):
                 if no_filesize_check:
-                    print chr(8) + "File {0} already exists.".format(file_name)
+                    print(chr(8) + "File {0} already exists.".format(file_name))
                     break
                 else:
                     local_file_size = os.path.getsize(file_name)
@@ -145,22 +148,22 @@ if not list_missing_only:
                 local_file_size = 0
             r = requests.get(SIGNED_DOWNLOAD_URL.format(machine_name), cookies=cookiejar)
             url = json.loads(r.text)["signed_url"]
-            u = urllib2.urlopen(url)
+            u = six.moves.urllib.request.urlopen(url)
             meta = u.info()
             file_size = int(meta.getheaders("Content-Length")[0])
             # Check if the file already exists and has the right size
             if local_file_size != file_size:
                 download_file(u, file_size, file_name)
             else:
-                print chr(8) + u"File {0} already exists and has correct size.".format(file_name)
+                print(chr(8) + u"File {0} already exists and has correct size.".format(file_name))
 
-    print "All downloads complete!"
+    print("All downloads complete!")
 
 # Lastly, check which files were missed due to strange humblebundle API
-print "Files which are still missing in the local library:"
+print("Files which are still missing in the local library:")
 for x in data["Downloads"]:
     for download in x["download_struct"]:
-        if "url" in download.keys():
+        if "url" in list(download.keys()):
             file_name = target_dir + target_folder_dict[x["machine_name"]] + "/" + download["url"]["web"]
             if not os.path.isfile(file_name):
-                print " - " + "/".join(file_name.split("/")[-2:])
+                print(" - " + "/".join(file_name.split("/")[-2:]))
